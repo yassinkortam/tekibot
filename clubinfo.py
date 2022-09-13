0819284822d47201872917288e289d1acaf62834
Yassin Kortam <yassinkortam@g.ucla.edu>
2022-09-12 18:29:21 -0700

    Create clubinfo.py

---
+++ clubinfo.py
@@ -0,0 +1,67 @@
+robotics = """Like what you see? This bot was made by me.
+Like to solve problems and create new things? Then this club’s the right one
+Join our discord and save the date, 
+no matter your skill level it's never too late. 
+With time and practice and countless opportunities to succeed, 
+we have got what you need. 
+Learn to code in python, make discord bots, and more,
+there is so much to explore. 
+Join now, join now, join now.
+"""
+
+clubs = {
+    "TEKI Robotics": 
+    {
+    "Icon":":man_technologist:",
+    "Contact":"Yassin Kortam", 
+    "Members": 19, 
+    "Discord Invite":"https://discord.gg/stCNeJdj",
+    "Poster":"club-posters/robotics.png"
+    },
+    'MUN': 
+    {
+    "Icon":":earth_africa:",
+    "Contact": "Aparna GB", 
+    "Members": 10, 
+    "Discord Invite":"",
+    },
+    "ASB": 
+    {
+    "Icon":":classical_building:",
+    "Contact":"Siri Tantry", 
+    "Members": 7, 
+    "Discord Invite":"",
+    }, 
+    "FIRSTS":
+    {
+    "Icon":":mortar_board:",
+    "Contact": "Siri Tantry",
+    "Members": "?",
+    "Discord Invite":"",
+    "Poster":"club-posters/firsts.png"
+    },
+    "Green Team":
+    {
+    "Icon":":leafy_green:",
+    "Contact":"Siri Tantry",
+    "Members": "?",
+    "Discord Invite": "",
+    "Poster":"club-posters/green.png"
+    },
+    "AMCCR":
+    {
+    "Icon":":robot:",
+    "Contact":"Anirudh Ramadurai",
+    "Members":"?",
+    "Discord Invite":"https://discord.com/channels/@me/750158849360527390/827369694323015740",
+    "Poster":"club-posters/amcr.png"
+    },
+    "STEAM Lab": 
+    {
+    "Icon":":school_satchel:",
+    "Contact": "Charmi Athuluru", 
+    "Members": 11, 
+    "Discord Invite":"https://discord.gg/78M4F2hNME",
+    "Poster":"club-posters/steam.png"
+    }
+    }
