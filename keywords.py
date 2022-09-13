ffdbb6ea28c11f7a4c41da19ad33004512ab596e
Yassin Kortam <yassinkortam@g.ucla.edu>
2022-09-12 18:28:43 -0700

    Create keywords.py

---
+++ keywords.py
@@ -0,0 +1,16 @@
+#List of all keywords and their weights (arbitrary based on personal guess)
+
+club_info_keywords = {'club': 2,'clubs': 2,'information': 1,'info':1, 'extracurriculars': 2, 'join': 1, 'activities': 1, 'activity': 1, 'accel': 1, 'amc': 1}
+   
+  #Instructions on how to join a club
+
+join_club_keywords = {'join':1,'find':1,'search':1, 'how': 1, 'instructions':1, 'process':1, 'joining':1, 'about': 1}
+
+#Lists all the clubs at accel
+existing_clubs_keywords = {'existing':2, 'current':2,'list': 2,'many':2, 'number':2, 'all': 2, 'are': 1, 'what': 1}
+
+#recommends clubs at accel
+recommend_clubs_keywords = {'should': 1, 'what': 1, 'like': 1, 'recommend': 2, 'look':1,'looking':1, 'good': 1}
+    
+#Gets information about a specific club
+info_clubs_keywords = {'information':2, 'info': 2, 'about':1, 'resources':1}
